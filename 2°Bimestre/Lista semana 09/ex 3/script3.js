91639fbbdeea6a0b58753c237a1560be750e3c16
andredg1 <andrediasgerez1@gmail.com>
2026-05-14 09:03:17 -0300

    Add files via upload

---
+++ 2°Bimestre/Lista semana 09/ex 3/script3.js
@@ -0,0 +1,30 @@
+let botao = document.getElementById("calcular");
+
+        botao.addEventListener("click", function() {
+
+            let nome = document.getElementById("nome").value;
+            let nota1 = Number(document.getElementById("nota1").value);
+            let nota2 = Number(document.getElementById("nota2").value);
+            let nota3 = Number(document.getElementById("nota3").value);
+            let media = (nota1 + nota2 + nota3) / 3;
+            let resultado = document.getElementById("resultado");
+            
+          if (media >= 7) {
+                resultado.innerHTML = nome + " foi APROVADO!<br>Média: " + media.toFixed(2);
+                resultado.style.color = "green";
+            }
+
+            else if (media >= 4) {
+                let falta = 10 - media;
+                resultado.innerHTML = nome + " está de EXAME!<br>Média: " + media.toFixed(2) +
+                "<br>Faltam " + falta.toFixed(2) + " pontos para chegar em 10.";
+
+                resultado.style.color = "yellow";
+            }
+
+            else {
+                resultado.innerHTML = nome + " foi REPROVADO!<br>Média: " + media.toFixed(2);
+                resultado.style.color = "red";
+            }
+
+        });

009a5a323f414cd78be90d411f820aeb86f1825a
andredg1 <andrediasgerez1@gmail.com>
2026-05-14 08:25:45 -0300

    Update script1.js

--- 2°Bimestre/Lista semana 09/ex 1/script1.js
+++ 2°Bimestre/Lista semana 09/ex 1/script1.js
@@ -11,9 +11,7 @@ function validarCPF() {
         return;
     }
 
-    // ======================
-    // PRIMEIRO DÍGITO
-    // ======================
+   
     let soma = 0;
 
     for (let i = 0; i < 9; i++) {
